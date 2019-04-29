9de6c2d831de4eb24f978d929e6312788cbf6092
dennis <dennismoha2@gmail.com>
2019-04-29 11:38:23 +0300

    first commit deploying the whole app although, user interface not made

---
+++ models/comments.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/Yelpcamps", {useNewUrlParser: true});
+
+var commentSchema = mongoose.Schema({
+	text : String,
+	author :  {
+		id : {			 
+			type: mongoose.Schema.Types.ObjectId,
+			//type : mongoose.Schema.types.ObjectId,
+			ref: "user"
+		}
+		
+	}
+
+});
+
+//var comment = mongoose.model("comment",commentSchema);
+
+
+module.exports = mongoose .model("comments", commentSchema);
